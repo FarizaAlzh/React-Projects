1fcbbc61e00e2c88b90bee332fbeed4770f76dab
FarizaAlzh <a_fariza@kbtu.kz>
2025-12-12 19:19:04 +0500

    delete folder

--- endterm-project/sample-app/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => (
-  <footer className="footer">
-    © {new Date().getFullYear()} BrightShop — Minimal demo. Built with ❤️
-  </footer>
-)
-
-export default Footer
--- endterm-project/sample-app/src/services/authService.js
+++
@@ -1,9 +0,0 @@
-import { signInWithEmailAndPassword, signOut as fbSignOut, onAuthStateChanged } from 'firebase/auth'
-import { auth } from '../firebaseConfig'
-
-export const login = (email, password) => signInWithEmailAndPassword(auth, email, password)
-export const logout = () => fbSignOut(auth)
-
-export const observeAuth = (cb) => {
-  return onAuthStateChanged(auth, (user) => cb(user))
-}
